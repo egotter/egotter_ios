8ee9dda152b8851175eac5c5c2ae1539a1bee10d
ts-3156 <ts_3156@yahoo.co.jp>
2020-02-17 10:56:45 +0900

    Initial Commit

---
+++ egotter_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  egotter_ios
+//
+//  Created by Teruki Shinohara on 2020/02/17.
+//  Copyright © 2020 Teruki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
